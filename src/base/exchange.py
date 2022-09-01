a9f3309a067a4262a6d651eb01f0e1a5de56d7ff
文 善郁 <mun_sunouk@cocone.co.jp>
2022-09-01 19:01:48 +0900

    update

--- src/base/exchange.py
+++ src/base/exchange.py
@@ -20,4 +20,16 @@ class Exchange(BaseExchange):
     def safe_value(dictionary, key, default_value=None):
         return dictionary[key] if Exchange.key_exists(dictionary, key) else default_value
     
+    @staticmethod
+    def key_exists(dictionary, key):
+        if dictionary is None or key is None:
+            return False
+        if isinstance(dictionary, list):
+            if isinstance(key, int) and 0 <= key and key < len(dictionary):
+                return dictionary[key] is not None
+            else:
+                return False
+        if key in dictionary:
+            return dictionary[key] is not None and dictionary[key] != ''
+        return False
     
