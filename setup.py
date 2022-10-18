1f612407db34259c9ab2090a58485f50e822a934
MunSunouk <52026496+MunSunouk@users.noreply.github.com>
2022-10-18 10:09:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(package_json, encoding='utf-8') as f:
 setup(
     
     name="ccdxt",
-    version="0.13",
+    version="0.14",
     
     description=package['description'],
     long_description=long_description,
