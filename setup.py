2cea112de57d7e8bab0ea971e78397c7d50b5f30
文 善郁 <mun_sunouk@cocone.co.jp>
2022-10-20 16:16:14 +0900

    test

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(package_json, encoding='utf-8') as f:
 setup(
     
     name="ccdxt",
-    version="0.24",
+    version="0.25",
     
     description=package['description'],
     long_description=long_description,
