94f8ace74ef536180c9df999bdc1a34727b4816f
文 善郁 <mun_sunouk@cocone.co.jp>
2023-12-03 19:52:09 +0900

    update megaton

--- exchange/mega.py
+++ exchange/mega.py
@@ -24,9 +24,6 @@ class Mega(Exchange):
             "exchangeName": "mega",
             "retries": 3,
             "retriesTime": 10,
-            # "host": 0,
-            # "account": None,
-            # "privateKey": None,
             "mnemonics": None,
             "log": None,
             "proxy": False,
@@ -35,6 +32,7 @@ class Mega(Exchange):
             "tonapi_url": "https://tonapi.io/",
             "api_url": "https://megaton.fi/",
             "sleep": 10,
+            "swap_address": None,
         }
 
         config.update(config_change)
@@ -43,12 +41,8 @@ class Mega(Exchange):
         self.id = 1
         self.chainName = config["chainName"]
         self.exchangeName = config["exchangeName"]
-        # self.addNounce = 0
         self.retries = config["retries"]
         self.retriesTime = config["retriesTime"]
-        # self.host = config["host"]
-        # self.account = config["account"]
-        # self.privateKey = config["privateKey"]
         self.mnemonics = config["mnemonics"]
         self.log = config["log"]
         self.proxy = config["proxy"]
@@ -58,8 +52,10 @@ class Mega(Exchange):
         self.api_url = config["api_url"]
         self.sleep = config["sleep"]
         self.gasPrice = None
+        self.swap_address = config["swap_address"]
 
         self.create_client()
+        self.create_wallet(self.mnemonics)
 
     async def fetch_balance(self, tokens: list):
 
@@ -123,18 +119,69 @@ class Mega(Exchange):
         self, amountA, tokenAsymbol, amountBMin, tokenBsymbol, path=None, *args, **kwargs
     ):
 
+        self.require(tokenAsymbol == tokenBsymbol, ValueError("Same Symbol"))
+
         await self.load_exchange(self.chainName, self.exchangeName)
 
         self.tokenSymbol = tokenAsymbol
         self.tokenBsymbol = tokenBsymbol
         self.amount = amountA
 
-        self.require(tokenAsymbol == tokenBsymbol, ValueError("Same Symbol"))
-
         tokenA = self.tokens[tokenAsymbol]
         tokenB = self.tokens[tokenBsymbol]
         amountA = self.from_value(value=amountA, exp=int(tokenA["decimals"]))
         amountBMin = 1
+        query_id = 0
+        forward_amount = to_nano(0.35, "ton")
+
+        if f"{tokenAsymbol}-{tokenBsymbol}" in self.pools:
+
+            pool = self.pools[f"{tokenAsymbol}-{tokenBsymbol}"]
+
+        elif f"{tokenBsymbol}-{tokenAsymbol}" in self.pools:
+
+            pool = self.pools[f"{tokenBsymbol}-{tokenAsymbol}"]
+
+        else:
+
+            ValueError(f"there is no pool available({tokenAsymbol}-{tokenBsymbol})")
+
+        router_address = self.markets["routerAddress"]
+        pool_address = pool["baseChain"][self.chainName][self.exchangeName]
+        tokenA_address = tokenA["contract"]
+        tokenB_address = tokenB["contract"]
+
+        body = self._create_swap_body(
+            Address(router_address),
+            Address(self.address),
+            Address(pool_address),
+            amountA,
+            Address(tokenA_address),
+            amountBMin,
+            Address(tokenB_address),
+            query_id,
+            forward_amount,
+        )
+
+        result = await self.create_transfer_message(body, Address(self.swap_address), 0.4)
+
+        return result
+
+    async def create_mint(
+        self,
+        token_address: Address,
+        jetton_amount: float,
+    ):
+
+        body = self._create_mint_body(
+            destination=Address(self.tool_wallet.address),
+            response_address=Address(self.tool_wallet.address),
+            jetton_amount=to_nano(0.1, "ton"),
+        )
+
+        result = await self.create_transfer_message(body, token_address, jetton_amount + 0.1)
+
+        return result
 
     def create_client(self):
 
@@ -152,7 +199,9 @@ class Mega(Exchange):
             tool_wallet.mnemonics, WalletVersionEnum(tool_wallet.version), 0
         )
 
-        raw_address = Address(tool_wallet.address).to_string(False, True, False)
+        self.address = tool_wallet.address
+
+        raw_address = Address(self.address).to_string(False, True, False)
 
         self.tool_wallet = tool_wallet
         self.sdk_wallet = sdk_wallet
@@ -256,23 +305,13 @@ class Mega(Exchange):
 
         return fee, reserveA, reserveB
 
-    async def create_mint(
-        self,
-        token_address: Address,
-        jetton_amount: float,
-    ):
-
-        body = self._create_mint_body(
-            destination=Address(self.tool_wallet.address),
-            response_address=Address(self.tool_wallet.address),
-            jetton_amount=to_nano(0.1, "ton"),
-        )
+    async def create_transfer_message(self, body, to_addr, jetton_amount):
 
         seqno = await self.tool_wallet.get_seqno()
 
         query = self.sdk_wallet.create_transfer_message(
-            to_addr=token_address,
-            amount=to_nano(jetton_amount + 0.1, "ton"),
+            to_addr=to_addr,
+            amount=to_nano(jetton_amount, "ton"),
             seqno=seqno,
             payload=body,
         )
@@ -281,13 +320,15 @@ class Mega(Exchange):
 
         await self.tool_wallet.provider.send_boc(jettons_boc)
 
-        result = (
-            (await self.tool_wallet.get_transactions(limit=1))[-1]
-            .out_msgs[0]
-            .to_dict_user_friendly()
-        )
+        return
 
-        return result
+        # result = (
+        #     (await self.tool_wallet.get_transactions(limit=1))[-1]
+        #     .out_msgs[0]
+        #     .to_dict_user_friendly()
+        # )
+
+        # return result
 
     def _create_mint_body(
         self,
@@ -323,9 +364,33 @@ class Mega(Exchange):
         minAmount: int,
         toJettonAddress: Address,
         query_id: int,
-        custom_payload: Cell,
         forward_amount: int = 0,
     ) -> Cell:
+
+        # forward_payload = self._create_swap_foraward_payload(
+        #     fromJettonAddress,
+        #     toJettonAddress,
+        #     minAmount,
+        # )
+
+        forward_payload = self._create_swap_foraward_payload(
+            query_id,
+            jetton_amount,
+            destination,
+            response_address,
+            forward_amount,
+            fromJettonAddress,
+            toJettonAddress,
+            minAmount,
+        )
+
+        hex_value = "b5ee9c72010101010002000000"
+
+        empty_cs = Cell.one_from_boc(hex_value).bits
+
+        # empty_cell = begin_cell().store_cell(empty_cs).end_cell()
+
+        # Cell.bytes_hash()
 
         body = Cell()  # jetton transfer
         body.bits.write_uint(0x0F8A7EA5, 32)
@@ -333,50 +398,60 @@ class Mega(Exchange):
         body.bits.write_grams(jetton_amount)
         body.bits.write_address(destination)
         body.bits.write_address(response_address)
-        body.bits.write_bit(custom_payload)
+        # body.bits.write_uint(0x178D4519, 32)
+        # body.refs.append(empty_cs)
+        # body.refs.append(empty_cell)
+        body.bits.write_string("EMPTY")
+        # body.write_cell(empty_cell)
+        # body.write_cell(empty_cs)
+        # body.bits.write_bytes(b"")
+        # body.bits.write_bytes(b"\x01")
+        # body.bits.write_bit_string(empty_cs)
         body.bits.write_grams(forward_amount)
-        body.bits.write_bytes(
-            self._create_swap_payload(
-                query_id,
-                jetton_amount,
-                pool_address,
-                response_address,
-                fromJettonAddress,
-                toJettonAddress,
-                minAmount,
-            )
-        )
+        body.refs.append(forward_payload)
+        return forward_payload
 
-        transfer_body = Cell()  # jetton internal transfer
-        transfer_body.bits.write_uint(0x178D4519, 32)
-        transfer_body.bits.write_uint(query_id, 64)
-        transfer_body.bits.write_grams(jetton_amount)
-        transfer_body.bits.write_address(response_address)
-        transfer_body.bits.write_address(response_address)
-        transfer_body.bits.write_grams(forward_amount)
+    # def _create_swap_foraward_payload(
+    #     self,
+    #     fromJettonAddress,
+    #     toJettonAddress,
+    #     minAmount,
+    # ):
 
-        body.refs.append(transfer_body)
-        return body
+    #     body = Cell()
+    #     body.bits.write_address(fromJettonAddress)
+    #     body.bits.write_address(toJettonAddress)
+    #     body.bits.write_grams(minAmount)
 
-    def _create_swap_payload(
+    #     return body
+
+    def _create_swap_foraward_payload(
         self,
         queryId,
         fromAmount,
-        pool_address,
+        destination,
         responseAddress,
+        forward_amount,
         fromJettonAddress,
         toJettonAddress,
         minAmount,
     ) -> Cell:
+
+        hex_value = "b5ee9c72010101010002000000"
+
+        empty_cs = Cell.one_from_boc(hex_value)
+
+        empty_cell = begin_cell().end_cell()
+
         return (
             begin_cell()
             .store_uint(0xF8A7EA5, 32)
             .store_uint(queryId, 64)
             .store_coins(fromAmount)
-            .store_address(pool_address)
+            .store_address(destination)
             .store_address(responseAddress)
-            .store_ref(begin_cell().end_cell())
-            .store_coins(to_nano(0.3))
+            .store_string("Empty")
+            .store_coins(forward_amount)
             .store_ref(
                 begin_cell()
                 .store_address(fromJettonAddress)
@@ -386,3 +461,15 @@ class Mega(Exchange):
             )
             .end_cell()
         )
+
+    def _create_swap_internal_transfer(
+        self, query_id, jetton_amount, response_address, forward_amount
+    ):
+        transfer_body = Cell()  # jetton internal transfer
+        transfer_body.bits.write_uint(0x178D4519, 32)
+        transfer_body.bits.write_uint(query_id, 64)
+        transfer_body.bits.write_grams(jetton_amount)
+        transfer_body.bits.write_address(response_address)
+        transfer_body.bits.write_address(response_address)
+        transfer_body.bits.write_grams(forward_amount)
+        transfer_body.bits.write_bit_string
